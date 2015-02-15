b7af67f0048ee440a2952c8abf4a38db86c13f9a
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-02-15 15:49:25 +0900

    change to print line number on error

--- qbin2hex.py
+++ qbin2hex.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import sys
 
+linenum=0
 while True:
 	try:
 		s=input()
 	except EOFError:
 		break
+
+	linenum+=1
 
 	mn=s.find(";")
 	if mn!=-1:
@@ -21,10 +24,10 @@ while True:
 
 	corestrlen=len(s)
 	if corestrlen!=64:
-		sys.exit("%s: error: input string core size is not 64 but %d"%(sys.argv[0], corestrlen))
+		sys.exit("%s: %d: error: input string core size is not 64 but %d"%(sys.argv[0], linenum, corestrlen))
 
 	b=int(s, 2)
 	if b>0xffffffffffffffff:
-		sys.exit("%s: error: input value is out of 64-bit unsigned integer range"%sys.argv[0])
+		sys.exit("%s: %d: error: input value is out of 64-bit unsigned integer range"%(sys.argv[0], linenum))
 
 	print("0x%08x, 0x%08x,"%(b&0x00000000ffffffff,(b&0xffffffff00000000)>>32))
